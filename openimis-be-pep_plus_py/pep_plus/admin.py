f40768e136db29aea6f6325a020fc1e8b17878bd
dsDomingossobrinho <88497747+DomingosSobrinhoDS@users.noreply.github.com>
2026-03-19 10:42:09 +0100

    update

--- openimis-be-pep_plus_py/pep_plus/admin.py
+++ openimis-be-pep_plus_py/pep_plus/admin.py
@@ -261,4 +261,3 @@ class CoordenacaoDistritalAdmin(admin.ModelAdmin):
     def num_tecnicos_operacionais(self, obj):
         return obj.tecnicos_operacionais.count()
     num_tecnicos_operacionais.short_description = 'Nº Técnicos Operacionais'
- 
